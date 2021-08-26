456665e58d8bcdc83a01cbc3d3bf7322c43e25b6
Matheus Nugas <matheusnugas@gmail.com>
2021-08-26 20:16:06 -0300

    Exercicio 12

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-12.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-12.js
@@ -6,13 +6,4 @@ for (let index = 0; index < numbers.length; index+=1) {
     multNumbers.push (numbers[index] * (numbers[index + 1] || 2))
 }
 
-
-// for (let index = 0; index < numbers.length; index+=1) {
-//     if (numbers[index] === undefined) {
-//         multNumbers.push(numbers[index] * 2);
-//     } else if (numbers[index] === "Number") {
-//         multNumbers.push (numbers[i] * numbers[i+1])
-//     }
-// }
-
 console.log(multNumbers)
