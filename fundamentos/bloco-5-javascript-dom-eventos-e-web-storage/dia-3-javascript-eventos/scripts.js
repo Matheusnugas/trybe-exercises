e7ba40de801eed535557e54272c3bcce1ca3eac7
Matheus Nugas <matheusnugas@gmail.com>
2021-09-08 21:53:03 -0300

    Exercicios feitos

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/scripts.js
@@ -137,3 +137,96 @@ function showFridays() {
 }
 
 showFridays();
+
+// Exercício 6:
+// Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
+
+function dayMouseOver() {
+  let day = document.querySelector("#days");
+  day.addEventListener("mouseover", function (event) {
+    event.target.style.fontWeight = "600";
+    event.target.style.fontSize = "30px";
+  });
+}
+
+function dayMouseOut() {
+  let day = document.querySelector("#days");
+  day.addEventListener("mouseout", function (event) {
+    event.target.style.fontWeight = "200";
+    event.target.style.fontSize = "20px";
+  });
+}
+
+dayMouseOver();
+dayMouseOut();
+
+// Exercício 7:
+// Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+function createNewTask(string) {
+  let listaTarefas = document.querySelector(".my-tasks");
+  let tarefa = document.createElement("span");
+  tarefa.innerHTML = string;
+  listaTarefas.appendChild(tarefa);
+}
+
+createNewTask("Cozinhar");
+
+// Exercício 8:
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+function createNewSubtitle(string) {
+  let listaTarefas = document.querySelector(".my-tasks");
+  let legenda = document.createElement("div");
+
+  legenda.className = "task";
+  legenda.style.backgroundColor = string;
+  listaTarefas.appendChild(legenda);
+}
+
+createNewSubtitle("yellow");
+
+// Exercício 9:
+// Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
+// Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+
+function selectTask() {
+  let task = document.querySelector(".task");
+  let selectedTask = document.getElementsByClassName(".task selected");
+
+  task.addEventListener("click", function (event) {
+    if (selectedTask.length === 0) {
+      event.target.className = "task selected";
+    } else {
+      event.target.className = "task";
+    }
+  });
+}
+
+selectTask();
+
+// Exercício 10:
+// Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+// Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+
+function setDayColor() {
+  let selectedTask = document.getElementsByClassName("task selected");
+  let days = document.querySelector("#days");
+  let taskDiv = document.querySelector(".task");
+  let taskColor = taskDiv.style.backgroundColor;
+
+  days.addEventListener("click", function (event) {
+    let eventTargetColor = event.target.style.color;
+    if (selectedTask.length > 0 && eventTargetColor !== taskColor) {
+      let color = selectedTask[0].style.backgroundColor;
+      event.target.style.color = color;
+    } else if (eventTargetColor === taskColor && selectedTask.length !== 0) {
+      event.target.style.color = "rgb(119,119,119)";
+    }
+  });
+}
+
+setDayColor();
