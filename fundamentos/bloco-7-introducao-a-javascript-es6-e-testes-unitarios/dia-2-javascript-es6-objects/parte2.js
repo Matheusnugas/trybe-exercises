89177d5660ce45d9f5c7f3f53511764d68c08bfc
Matheus Nugas <matheusnugas@gmail.com>
2021-09-27 18:28:56 -0300

    Exercicios 4, 5 e 6 parte 2 feitos

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte2.js
@@ -32,4 +32,23 @@ let listObjectKeys = object => Object.keys(object);
 
 let showObjectLength = object => Object.keys(object).length;
 
-console.log(showObjectLength(lesson3))
+// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+
+let showObjectValues = object => Object.values(object);
+
+// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+
+// 6 - Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+let studentTotal = object => {
+    return object.lesson1.numeroEstudantes + object.lesson2.numeroEstudantes + object.lesson3.numeroEstudantes;
+}
+console.log (studentTotal(allLessons))
+
+
+
+
+
