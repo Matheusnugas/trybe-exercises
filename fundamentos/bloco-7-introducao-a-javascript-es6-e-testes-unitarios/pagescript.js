b19dfba422f83226cc5a8e809507db45d59a9cd8
Matheus Nugas <matheusnugas@gmail.com>
2021-09-27 15:36:38 -0300

    Exercicios 7.1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/pagescript.js
+++
@@ -1,9 +0,0 @@
-const counterButton = document.getElementById('counter-button');
-const counterText = document.getElementById('counter');
-let clicks = 0;
-
-
-counterButton.onclick = () => {
-    clicks += 1;
-    counterText.innerHTML = clicks;
-}
