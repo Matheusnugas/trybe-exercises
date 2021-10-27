e41f5e252ded1371d28c2db3578cbb5e0b2f3d1e
Matheus Nugas <matheusnugas@gmail.com>
2021-10-27 19:34:07 -0300

    Requisito 3+ feitos

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/second-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './header.jsx'
+import Content from './content.jsx'
+import Footer from './footer.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
