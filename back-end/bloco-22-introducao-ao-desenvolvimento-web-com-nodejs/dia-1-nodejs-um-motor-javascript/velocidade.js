2c25cc06ec51f4f11ed16532f54292a0a45f1c2f
Matheus Nugas <matheusnugas@gmail.com>
2022-03-12 14:47:28 -0300

    Exercicio 6 feito

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/velocidade.js
@@ -0,0 +1,10 @@
+const readlineSync = require('readline-sync');
+
+const dist = readlineSync.questionInt('Qual a distancia percorrida em metros? ');
+const time =  readlineSync.questionInt('Qual o tempo gasto em segundos? ');
+
+const averageSpeed = () => {
+    console.log(`A velocidade média é de ${(dist/time).toFixed(2)} m/s`);
+}
+
+averageSpeed()
