0abbe82fa9987a45a1bf2cfb935546f629703f75
Matheus Nugas <matheusnugas@gmail.com>
2022-03-14 11:28:32 -0300

    Exercicio 1 added

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/numbers.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/numbers.js
@@ -8,11 +8,12 @@ const numbers = (num1, num2, num3) => {
       reject(new Error("Somente números"));
     }
     const result = (num1 + num2) * num3;
+    if (result < 50) return reject(new Error("Valor muito baixo"));
     resolve(result);
   });
   return promise;
 };
 
-numbers(3, "a", 2)
+numbers(40, 1, 2)
   .then((result) => console.log(`sucesso: ${result}`))
   .catch((err) => console.log(`erro: ${err.message}`));
