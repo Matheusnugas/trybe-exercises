c5bbde0db427d6774d36a04d0141c6e8b0214e69
Matheus Nugas <matheusnugas@gmail.com>
2022-03-17 11:19:19 -0300

    Exercicio 1 feito

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/ping.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+app.listen(3000, () => {
+  console.log("Aplicação rodando na porta 3000");
+});
