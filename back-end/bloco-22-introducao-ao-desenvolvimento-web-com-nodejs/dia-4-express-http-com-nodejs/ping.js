e24e242603b531bad9d438123676f89feded72e5
Matheus Nugas <matheusnugas@gmail.com>
2022-03-17 18:24:43 -0300

    Exercicios

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/ping.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/ping.js
@@ -1,8 +1,36 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 app.get("/ping", (req, res) => {
   res.json({ message: "pong" });
+});
+
+app.post("/hello", (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+app.post("/hello", (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ message: `Hello, ${name}!` });
+});
+
+app.post("/greetings", (req, res) => {
+  const { name, age } = req.body;
+  if (age >= 17) {
+    res.status(200).json({ message: `Hello, ${name}` });
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+});
+
+app.put("/users/:name/:age", (req, res) => {
+  const { name, age } = req.params;
+  res
+    .status(200)
+    .json({ message: `You are ${name} and you are ${age} years old` });
 });
 
 app.listen(3000, () => {
